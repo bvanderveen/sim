381d2a0ee3a59e0fc43c41f0cf0e7bae2cf6efe8
Benjamin van der Veen <b@bvanderveen.com>
2012-01-27 00:52:10 -0800

    thrust direction as quaternion. math doesn't seem quite right yet, but maybe almost.

--- sim/sim.c
+++ sim/sim.c
@@ -16,21 +16,47 @@ typedef struct Sim3Vector Sim3Vector;
 #define Sim3VectorMake(X, Y, Z) ((Sim3Vector){ .x = (X), .y = (Y), .z = (Z) })
 #define Sim3VectorAdd(A, B) ((Sim3Vector){ .x = (A).x + (B).x, .y = (A).y + (B).y, .z = (A).z + (B).z })
 #define Sim3VectorScale(K, V) ((Sim3Vector){ .x = (K) * (V).x, .y = (K) * (V).y, .z = (K) * (V).z })
-//#define Sim3VectorMagnitudeSquared(V) ((V).x * (V).x + (V).y * (V).y + (V).z * (V).z)
-//#define Sim3VectorRotate(R, V) ((Sim3Vector){ .x =  })
+
+struct SimQuat {
+  SimUnit w, x, y, z;
+};
+typedef struct SimQuat SimQuat;
+
+#define SimQuatPrint(V) printf("w = %f, x = %f, y = %f, z = %f\n", (V).w, (V).x, (V).y, (V).z)
+#define SimQuatMake(W, X, Y, Z) ((SimQuat){ .w = (W), .x = (X), .y = (Y), .z = (Z) })
+#define SimQuatZero SimQuatMake(0,0,0,0)
+#define SimQuatConjugate(Q) ((SimQuat){ .w = (Q).w, .x = -(Q).x, .y = -(Q).y, .z = -(Q).z })
+#define SimQuatMult(L, R) ((SimQuat){ \
+  .w = (L).w * (R).w - (L).x * (R).x - (L).y * (R).y - (L).z * (R).z, \
+  .x = (L).w * (R).x + (L).x * (R).w + (L).y * (R).z - (L).z * (R).y, \
+  .y = (L).w * (R).y + (L).y * (R).w + (L).z * (R).x - (L).x * (R).z, \
+  .z = (L).w * (R).z + (L).z * (R).w + (L).x * (R).y - (L).y * (R).x })
+
+Sim3Vector SimQuatRotate(SimQuat q, Sim3Vector v) {
+  printf("input was\n");
+  Sim3VectorPrint(v);
+  SimQuat left = SimQuatMult(q, SimQuatMake(0, v.x, v.y, v.z));
+  SimQuat result = SimQuatMult(left, SimQuatConjugate(q));
+  Sim3Vector outV = Sim3VectorMake(result.x, result.y, result.z);
+  printf("output was");
+  SimQuatPrint(result);
+  return outV;
+}
 
 struct SimThruster {
   SimUnit amount;
-  Sim3Vector direction;
+  SimQuat direction;
 };
 typedef struct SimThruster *SimThrusterRef;
 
 SimThrusterRef SimThrusterCreate() {
   SimThrusterRef result = (SimThrusterRef)malloc(sizeof(struct SimThruster));
   result->amount = SimUnitZero;
-  result->direction = Sim3VectorZero;
+  result->direction = SimQuatZero;
   return result;
 }
+
+#define SimThrusterGetThrust(T) SimQuatRotate((T)->direction, Sim3VectorMake((T)->amount,0,0))
 
 void SimThrusterDestroy(SimThrusterRef ref) {
   free(ref);
@@ -39,9 +65,6 @@ void SimThrusterDestroy(SimThrusterRef ref) {
 struct SimVehicle {
   Sim3Vector position;
   Sim3Vector velocity;
-
-  Sim3Vector angle;
-  Sim3Vector angularVelocity;
 
   SimUnit mass;
 
@@ -55,12 +78,10 @@ SimVehicleRef SimVehicleCreate() {
   result->position = Sim3VectorZero;
   result->velocity = Sim3VectorZero;
 
-  result->angle = Sim3VectorZero;
-  result->angularVelocity = Sim3VectorZero;
-
   result->mass = 1;
   result->thruster = SimThrusterCreate();
-  result->thruster->amount = .1;
+  result->thruster->amount = .05;
+  result->thruster->direction = SimQuatMake(1,1,1,1);
   return result;
 }
 
@@ -73,7 +94,7 @@ void SimVehicleUpdate(SimVehicleRef ref, SimUnit t) {
   ref->position = Sim3VectorAdd(ref->position, Sim3VectorScale(t, ref->velocity));
 
   // XXX need to rotate thrust vector.
-  Sim3Vector thrust = Sim3VectorMake(0, ref->thruster->amount, 0);
+  Sim3Vector thrust = SimThrusterGetThrust(ref->thruster);
   ref->velocity = Sim3VectorAdd(ref->velocity, Sim3VectorScale(1/ref->mass, thrust));
 }
 
